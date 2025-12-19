279d44ca644131079359c4a8a1e2618ad51978df
David Olukunle <olukunledavid24@gmail.com>
2025-12-19 09:30:50 +0100

    render hosting

--- auth/auth_utils.py
+++ auth/auth_utils.py
@@ -1,19 +1,23 @@
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import jwt
+import os
+from dotenv import load_dotenv
 
-# Password hashing config
-pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+load_dotenv()
 
-# JWT config (KEEP SECRET IN REAL PROJECT)
-SECRET_KEY = "supersecretkey"
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
